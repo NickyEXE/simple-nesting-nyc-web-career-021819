0789cc91c46890e1ba27770b2fd644c5fab9332b
Nicholas Dover <dovern42@gmail.com>
2019-02-06 05:28:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :known_for => "Unix",
           :languages => ["C"]
         }
-				programmer_hash[:dennis_ritchie][:languages]
+				programmer_hash[:dennis_ritchie][:languages].first
      }
 end
 
